9505b5ff7b572d48005f3b0dca619c3a39055bf0
rubic <adamtheball@gmail.com>
2020-05-03 17:58:46 -0700

    items grid now has the right html

--- react-frontend/src/comps/itemGrid.js
+++ react-frontend/src/comps/itemGrid.js
@@ -1,30 +1,43 @@
 import React, {useState, useEffect} from 'react';
 
+const ItemGridElem = ({id, name, description, price, quantity}) => {
+    return (
+        <div className="item" key={"item" + id}>
+            <img src="test_square.png"></img>
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <p>${price}</p>
+            <p>Quantity: {quantity}</p>
+        </div>
+    );
+}
+
 /**
  * Represents an entire island that contains all of the items in the database.
  */
 const ItemGridcomp = () => {
-    const [message, setMessage] = useState("");
+    const [allItemsHtml, setAllItemsHtml] = useState([]);
 
     useEffect(() => {
         fetch('/api/items', {
             accept: "application/json"
         })
-        .then(response => response.text())
-        .then(message => { setMessage(message); });
+        .then(res => {
+            if(!res.ok) throw new Error(res);
+            return res.json();
+        })
+        .then(res => {
+            res = res.map(ItemGridElem);
+            setAllItemsHtml(res);
+        })
+        .catch((error) => {
+            setAllItemsHtml(<p>{error}</p>);
+        });
     },[]);
 
     return (
         <div className="island">
-            {message}
-        </div>
-    );
-}
-
-const ItemGridElem = () => {
-    return (
-        <div className="item">
-            <p>awesome</p>
+            {allItemsHtml}
         </div>
     );
 }
